c8dc680432cc53bb0d6abe172dcf6737c82f9e07
juko-0 <juko20050828@gmail.com>
2026-06-27 10:28:33 +0545

    final solution

--- 3.js
+++ 3.js
@@ -1,18 +1,35 @@
 //3739. Count Subarrays With Majority Element II
-let nums =[1,2,2,3]
-let target = 2;
-let count = 0;
-let arr = [];
-let result = 0;
-for (let i = 0; i < nums.length; i++) {
-for(let j=i; j<nums.length; j++){
-   arr[j] = nums[j];
-    if(arr[j] === target){
-        count++;
-    console.log(arr);
-    }
-}
-arr.length=0;
-}
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var countMajoritySubarrays = function(nums, target) {
+    let count = 0;
+    let arr = [];
+    let length = 0;
+    let result = 0;
 
-console.log(count);
+    for (let i = 0; i < nums.length; i++) {
+        arr = [];
+
+        for (let j = i; j < nums.length; j++) {
+            arr.push(nums[j]);
+
+            count = 0;
+            length = arr.length;
+
+            for (let item of arr) {
+                if (item === target) {
+                    count++;
+                }
+            }
+
+            if (count > Math.floor(length / 2)) {
+                result++;
+            }
+        }
+    }
+
+    return result;
+};
