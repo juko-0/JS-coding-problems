df4d700774b383b9a22d79c9fdcec269179bec60
juko-0 <juko20050828@gmail.com>
2026-07-13 21:56:14 +0545

    my algorithm won't solve the problem

--- 11.js
+++ 11.js
@@ -6,10 +6,11 @@
 var maxArea = function(height) {
    let max = 0,dis=0,k;
        let indexed = height.map((value, index) => [value, index + 1]);
-       let sorted = [...indexed].sort((a, b) => b[0] - a[0]);
+       let sorted ;
        console.log(indexed)
-       console.log(sorted);
    for(let i =1; i<indexed.length; i++){
+      sorted = [...indexed].sort((a, b) => b[0] - a[0]);
+      console.log(sorted);
       for(let j=0; j<i; j++){
          if(Math.abs(sorted[j][1]-sorted[i][1])>dis){
          dis = Math.abs(sorted[j][1]-sorted[i][1]);
@@ -19,11 +20,11 @@ var maxArea = function(height) {
    if(sorted[i][0]*dis>max){  
    max = sorted[i][0]*dis;
    }
-   (sorted[i-1][1]<sorted[i][1])?indexed.splice(sorted[i-1][1], sorted[i][1]-2) :indexed.splice(sorted[i][1],sorted[i-1][1]-2);
+   (sorted[i-1][1]<sorted[i][1])?indexed.splice(sorted[i-1][1], dis-1) :indexed.splice(sorted[i][1],dis-1);
    }
    return max;
    
 };
 
-let height = [1,8,6,2,5,4,8,25,7];
+let height = [1,2,3,4,5,6,7,8,9,10];
 console.log(maxArea(height));
