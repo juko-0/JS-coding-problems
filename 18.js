ea07a37b063b730b6c18c150b0829b51b2ce06c9
juko-0 <juko20050828@gmail.com>
2026-08-03 14:54:49 +0545

    my solution

--- 18.js
+++ 18.js
@@ -4,31 +4,53 @@
  * @return {number[][]}
  */
 var fourSum = function(nums, target) {
+    nums.sort((a, b) => a - b);
+
+    // Keep at most 4 copies of each value
+    let temp = [];
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i === 0 || nums[i] !== nums[i - 1]) {
+            count = 1;
+            temp.push(nums[i]);
+        } else if (count < 4) {
+            temp.push(nums[i]);
+            count++;
+        }
+    }
+
     const result = [];
     const current = [];
     const seen = new Set();
-    let backtrack = (start)=>{
+
+    function backtrack(start) {
         if (current.length === 4) {
-            let sum = current[0]+current[1]+current[2]+current[3];
-            if(sum===target){
-                const sorted = [...current].sort((a,b)=>a-b);
-                const key = JSON.stringify(sorted);
-        if (!seen.has(key)) {
-            seen.add(key);
-            result.push(sorted);
-        }
+            const sum =
+                current[0] +
+                current[1] +
+                current[2] +
+                current[3];
+
+            if (sum === target) {
+                const key = JSON.stringify(current);
+
+                if (!seen.has(key)) {
+                    seen.add(key);
+                    result.push([...current]);
+                }
             }
             return;
         }
-        for (let i = start; i < nums.length; i++) {
-            current.push(nums[i]);
+
+        for (let i = start; i < temp.length; i++) {
+            current.push(temp[i]);
             backtrack(i + 1);
             current.pop();
         }
     }
+
     backtrack(0);
+
     return result;
 };
-let nums =[2,2,2,2];
-target = 8;
-console.log(fourSum(nums, target));
