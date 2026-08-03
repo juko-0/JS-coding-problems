e415efac369ab26fe13b55a6f305246981fa0fdc
juko-0 <juko20050828@gmail.com>
2026-08-03 14:27:42 +0545

    exceeds time limit

--- 18.js
+++ 18.js
@@ -1,53 +1,34 @@
-//4Sum
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[][]}
  */
 var fourSum = function(nums, target) {
-    let len = nums.length;
-    let array2=[];
-    let sum;
-    let check;
-    let count = 0;
-    if(len<4){
-        return []
-    }else{
-    for(let i=0;i<len;i++){
-        let j = i+1%len;
-        if(i<(i+3)%len){
-            while(j>0 && (j+2)%len!==i){
-            sum = nums[i]+nums[j%len]+nums[(j+1)%len]+nums[(j+2)%len];
-            
+    const result = [];
+    const current = [];
+    const seen = new Set();
+    let backtrack = (start)=>{
+        if (current.length === 4) {
+            let sum = current[0]+current[1]+current[2]+current[3];
             if(sum===target){
-                check = [nums[i],nums[j%len],nums[(j+1)%len],nums[(j+2)%len]].sort((a,b)=>a-b);
-                let checkString = JSON.stringify(check);
-                let exists = array2.some(item => JSON.stringify(item) === checkString);
-                if(!exists){
-                array2[count]=[nums[i],nums[j%len],nums[(j+1)%len],nums[(j+2)%len]].sort((a,b)=>a-b);
-                }
-                count++;
+                const sorted = [...current].sort((a,b)=>a-b);
+                const key = JSON.stringify(sorted);
+        if (!seen.has(key)) {
+            seen.add(key);
+            result.push(sorted);
+        }
             }
-            j=(j+1)%len;
-            }
-        }else{
-            while((j+2)%len!==(i-1)){
-                           sum = nums[i]+nums[j%len]+nums[(j+1)%len]+nums[(j+2)%len];
-            if(sum===target){
-                check = [nums[i],nums[j%len],nums[(j+1)%len],nums[(j+2)%len]].sort((a,b)=>a-b);
-                let checkString = JSON.stringify(check);
-                let exists = array2.some(item => JSON.stringify(item) === checkString);
-                if(!exists){
-                array2[count]=[nums[i],nums[j%len],nums[(j+1)%len],nums[(j+2)%len]].sort((a,b)=>a-b);
-                }
-                count++;
-            }
-            j=(j+1)%len; 
-            }
+            return;
+        }
+        for (let i = start; i < nums.length; i++) {
+            current.push(nums[i]);
+            backtrack(i + 1);
+            current.pop();
         }
     }
-    return array2;
-}};
-let nums =[-3,-1,0,2,4,5];
-target = 0;
+    backtrack(0);
+    return result;
+};
+let nums =[2,2,2,2];
+target = 8;
 console.log(fourSum(nums, target));
