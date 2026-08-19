313db2988e13d4fa1f2abbd727f24a4205bef418
juko-0 <juko20050828@gmail.com>
2026-08-19 21:01:46 +0545

    first commit

--- 24.js
+++ 24.js
@@ -1,29 +1,37 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-var swapPairs = function(head) {
-let dummy = {next : head};
-let prev = dummy;
-while(prev.next!==null && prev.next.next!==null){
-    let first = prev.next;
-    let second = first.next;
-
-    first.next = second.next;
-    second.next = first;
-
-    prev.next = second;
-
-    prev = first;
-
+function Node(value) {
+    this.value = value;
+    this.next = null;
 }
+let a = new Node(10);
+let b = new Node(20);
+let c = new Node(3);
+let d = new Node(40);
+ 
+a.next = b;
+b.next= c;
+c.next =d;
 
-return dummy.next;
+var swapPairs = function(head) {
+    let dummy = { next: head };
+    let prev = dummy;
+
+    while (prev.next !== null && prev.next.next !== null) {
+        let first = prev.next;
+        let second = first.next;
+
+        // Swap
+        first.next = second.next;
+        second.next = first;
+
+        // Connect previous part to swapped pair
+        prev.next = second;
+
+        // Move to the next pair
+        prev = first;
+    }
+
+    return dummy.next;
 };
+
+console.log(swapPairs(a));
+
