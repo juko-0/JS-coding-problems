b586cb98114aabae1531b66f7e18e4a90863b30d
juko-0 <juko20050828@gmail.com>
2026-08-19 13:21:46 +0545

    first commit

---
+++ 24.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+let dummy = {next : head};
+let prev = dummy;
+while(prev.next!==null && prev.next.next!==null){
+    let first = prev.next;
+    let second = first.next;
+
+    first.next = second.next;
+    second.next = first;
+
+    prev.next = second;
+
+    prev = first;
+
+}
+
+return dummy.next;
+};
