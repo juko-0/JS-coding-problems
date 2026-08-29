72e5e7bcb2b3ad2ffdc17698a0433ec0ebc68f90
juko-0 <juko20050828@gmail.com>
2026-08-29 10:57:15 +0545

    error case 1

--- 19.js
+++ 19.js
@@ -11,26 +11,19 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    let x =1;
-    let node =head;
-    let first = {next:head}
-    while(head.next!==null){
-        head = head.next;
-        x++;
-    }
-    if(x-n!==0){
-    for(i=1;i<x-n;i++) node=node.next;
-    if(node.next!==null){
-        if(node.next.next!==null){
-            node.next=node.next.next;
-        }else{
-            node.next=null;
-        }
-    }else{
-        node.next=null;
-    }
-    }else{
-        first.next=null;
-    }
-    return first.next;
+ head = {next : head};
+ let temp = head;
+ let node =head;
+ let x =0;
+ while(temp.next!==null){
+    temp=temp.next;
+    x++;
+ }
+ let i = x-n;
+ for(let y=0; y<i; y++){
+ node=node.next;
+ }
+ console.log(i+2<x);
+ (i+2<x)?node.next=node.next.next:node.next=null;
+ return head.next;
 };
