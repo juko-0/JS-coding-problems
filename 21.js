57f7d6eca019c1d7e6160b8fa342ba79e9ed2954
juko-0 <juko20050828@gmail.com>
2026-08-30 15:48:05 +0545

    error line 24

--- 21.js
+++ 21.js
@@ -11,5 +11,29 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    
+    let node = new ListNode();
+    let head = {next : node};
+    while(list1!==null || list2!==null){
+    if(list1===null) node=list2;
+    else if (list2===null) node = list1;
+    else{
+    if(list1.val===list2.val){
+    node.val = list1.val;
+    list1= list1.next;
+    node = node.next;
+    node.val = list2.val;
+    list2 = list2.next;
+    node = node;
+    }if(list1.val>list2.val){
+        node.val=list2.val;
+        list2 = list2.next;
+        node = node.next;
+    }else{
+        node.val=list1.val;
+        list1=list1.next;
+        node=node.next;
+    }
+}
+    }
+    return head.next;
 };
